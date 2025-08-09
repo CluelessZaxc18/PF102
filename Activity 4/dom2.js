c99c91616603b0e4a48c2b9e1d492b9fd8944f8d
Roco <ZaxcKheal@gmail.com>
2025-08-09 15:57:13 +0800

    Adding this shit

---
+++ Activity 4/dom2.js
@@ -0,0 +1,12 @@
+
+const myButton = document.getElementById("myButton");
+const mylabel = document.getElementById("mylabel");
+
+myButton.onclick = function() { 
+let randomNum;
+let max = 6;
+let min = 1;
+
+randomNum = Math.trunc(Math.random() * max) + min;
+mylabel.textContent = Generated number : ${randomNum};
+}
