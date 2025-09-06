b19f1d1fa463d0e560f8dc6d01b4f8b233d1b962
Roco <ZaxcKheal@gmail.com>
2025-09-06 11:48:40 +0800

    Adding

--- Activity 4/dom2.js
+++ Activity 4/dom2.js
@@ -1,6 +1,7 @@
-
 const myButton = document.getElementById("myButton");
 const mylabel = document.getElementById("mylabel");
+const mylabel2 = document.getElementById("mylabel2");
+const mylabel3 = document.getElementById("mylabel3");
 
 myButton.onclick = function() { 
 let randomNum;
@@ -10,4 +11,9 @@ let min = 1;
 randomNum = Math.trunc(Math.random() * max) + min;
 mylabel.textContent = randomNum;
 
+randomNum = Math.trunc(Math.random() * max) + min;
+mylabel2.textContent = randomNum;
+
+randomNum = Math.trunc(Math.random() * max) + min;
+mylabel3.textContent = randomNum;
 }
